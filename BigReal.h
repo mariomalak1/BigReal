dd9e29bb551cdacfb4f8a80a819c7181c60b6b80
Ehab Ashraf <102333950+Ehabp1@users.noreply.github.com>
2022-11-04 01:21:32 +0200

    Add files via upload

--- BigReal.h
+++ BigReal.h
@@ -12,8 +12,8 @@ such as: +, -, <, and >.
 
 #ifndef BIGREALV3_BIGREAL_H
 #define BIGREALV3_BIGREAL_H
-
 #include "BigDecimalIntClass.h"
+
 
 class BigReal {
 private:
@@ -24,21 +24,21 @@ private:
 public:
     BigReal (double realNumber = 0.0); // Default constructor
     BigReal (string realNumber);
+    bool   checkValidInput(string realNumber);
     BigReal (BigDecimalInt bigInteger);
-    bool checkValidInput(string realNumber) ;
     BigReal (const BigReal& other); // Copy constructor
     BigReal (BigReal&& other); // Move constructor
     BigReal& operator= (BigReal& other); // Assignment operator
     BigReal& operator= (BigReal&& other); // Move assignment
     BigReal operator+ (BigReal& other);
     BigReal operator- (BigReal& other);
-    bool operator< (BigReal anotherReal);
-    bool operator> (BigReal anotherReal);
-    bool operator== (BigReal anotherReal);
+    bool operator< (BigReal &anotherReal);
+    bool operator> (BigReal &anotherReal);
+    bool operator== (BigReal &anotherReal);
     int size();
     int sign();
-    friend ostream& operator << (ostream& out, BigReal num);
-    friend istream& operator >> (istream& out, BigReal num);
+    friend ostream& operator << (ostream& out, BigReal& num);
+    friend istream& operator >> (istream& in, BigReal& num);
 };
 
 
